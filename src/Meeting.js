facabdb1d76df979999bc4cc7b14e30ac64876f3
Ali Madooei <madooei@jhu.edu>
2020-11-19 10:10:14 -0500

    Step 8

--- src/Meeting.js
+++ src/Meeting.js
@@ -1,9 +1,13 @@
 import "./Meeting.css";
+import QRCode from "qrcode.react";
 
 function Meeting(props) {
   const { meeting, key } = props;
   return (
     <li className="meeting-list-item" key={key}>
+      <div>
+        <QRCode value={meeting.link} />
+      </div>
       <div className="meeting-details">
         <p className="title">{meeting.course}</p>
         <p>{meeting.instructor}</p>
