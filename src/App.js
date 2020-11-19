4273bd3a96deadbd791d7556876f607e4f8eeb06
Ali Madooei <madooei@jhu.edu>
2020-11-19 09:46:43 -0500

    Step 4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
-const meetings = [
-  {
-    _id: "5faf5ab7043e87536e31e54e",
-    course: "EN.601.226 Data Structure",
-    instructor: "Ali Madooei",
-    time: "MWF 12:00 - 1:15 PM",
-    link: "https://wse.zoom.us/j/91907049828",
-  },
-  {
-    _id: "5faf5ab7043e87536e31e54f",
-    course: "EN.601.226 Data Structure",
-    instructor: "Ali Madooei",
-    time: "MWF 1:30 - 2:45 PM",
-    link: "https://wse.zoom.us/j/99066784665",
-  },
-  {
-    _id: "5faf5ab7043e87536e31e550",
-    course: "EN.601.280 Full-Stack JavaScript",
-    instructor: "Ali Madooei",
-    time: "TuTh 12:00 - 1:15 PM",
-    link: "https://wse.zoom.us/j/93926139464",
-  },
-  {
-    _id: "5faf5ab7043e87536e31e551",
-    course: "EN.601.280 Full-Stack JavaScript",
-    instructor: "Ali Madooei",
-    time: "TuTh 1:30 - 2:45 PM",
-    link: "https://wse.zoom.us/j/91022779135",
-  },
-];
+import { Component } from "react";
 
-function App() {
-  return <div style={{ color: "white" }}>{JSON.stringify(meetings)}</div>;
+class App extends Component {
+  state = {
+    meetings: [
+      {
+        _id: "5faf5ab7043e87536e31e54e",
+        course: "EN.601.226 Data Structure",
+        instructor: "Ali Madooei",
+        time: "MWF 12:00 - 1:15 PM",
+        link: "https://wse.zoom.us/j/91907049828",
+      },
+      {
+        _id: "5faf5ab7043e87536e31e54f",
+        course: "EN.601.226 Data Structure",
+        instructor: "Ali Madooei",
+        time: "MWF 1:30 - 2:45 PM",
+        link: "https://wse.zoom.us/j/99066784665",
+      },
+      {
+        _id: "5faf5ab7043e87536e31e550",
+        course: "EN.601.280 Full-Stack JavaScript",
+        instructor: "Ali Madooei",
+        time: "TuTh 12:00 - 1:15 PM",
+        link: "https://wse.zoom.us/j/93926139464",
+      },
+      {
+        _id: "5faf5ab7043e87536e31e551",
+        course: "EN.601.280 Full-Stack JavaScript",
+        instructor: "Ali Madooei",
+        time: "TuTh 1:30 - 2:45 PM",
+        link: "https://wse.zoom.us/j/91022779135",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div style={{ color: "white" }}>
+        {JSON.stringify(this.state.meetings)}
+      </div>
+    );
+  }
 }
 
 export default App;
